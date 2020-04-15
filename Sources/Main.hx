a03bd0c0cdd7c66547dbe75cae3da0ab54ec922a
Robert Konrad <git2016@robdangero.us>
2020-04-15 18:58:16 +0200

    Add proper test code

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -5,6 +5,7 @@ import kha.Color;
 import kha.Framebuffer;
 import kha.Scheduler;
 import kha.System;
+import nanovg.NVG;
 
 class Main {
 	static function update(): Void {
@@ -13,7 +14,13 @@ class Main {
 	static function render(frames: Array<Framebuffer>): Void {
 		final g = frames[0].g2;
 		g.begin(true, Color.fromBytes(0, 95, 106));
-		nanovg.NVG.nvg__sqrtf(1);
+
+		var vg: NVGcontext = NVG.nvgCreateKha(NVGcreateFlags.NVG_ANTIALIAS | NVGcreateFlags.NVG_STENCIL_STROKES);
+		NVG.nvgBeginPath(vg);
+		NVG.nvgRect(vg, 100,100, 120,30);
+		NVG.nvgFillColor(vg, NVG.nvgRGBA(255,192,0,255));
+		NVG.nvgFill(vg);
+
 		g.end();
 	}
 
