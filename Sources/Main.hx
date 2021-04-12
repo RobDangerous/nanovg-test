7be5b171f8dae0c82e8412c075ad128bbbe0bdf4
Robert Konrad <git2016@robdangero.us>
2021-04-12 02:15:04 +0200

    Format code

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -9,8 +9,7 @@ import nanovg.KhaContext;
 import nanovg.NVG;
 
 class Main {
-	static function update(): Void {
-	}
+	static function update(): Void {}
 
 	static function render(frames: Array<Framebuffer>): Void {
 		final g = frames[0].g4;
@@ -22,8 +21,8 @@ class Main {
 		context.g = g;
 		NVG.nvgBeginFrame(vg, frames[0].width, frames[0].height, 1.0);
 		NVG.nvgBeginPath(vg);
-		NVG.nvgRect(vg, 100,100, 120,30);
-		NVG.nvgFillColor(vg, NVG.nvgRGBA(255,192,0,255));
+		NVG.nvgRect(vg, 100, 100, 120, 30);
+		NVG.nvgFillColor(vg, NVG.nvgRGBA(255, 192, 0, 255));
 		NVG.nvgFill(vg);
 		NVG.nvgEndFrame(vg);
 
@@ -31,10 +30,10 @@ class Main {
 	}
 
 	public static function main() {
-		System.start({title: "nanovg", width: 1024, height: 768}, function (_) {
-			Assets.loadEverything(function () {
-				Scheduler.addTimeTask(function () { update(); }, 0, 1 / 60);
-				System.notifyOnFrames(function (frames) { render(frames); });
+		System.start({title: "nanovg", width: 1024, height: 768}, function(_) {
+			Assets.loadEverything(() -> {
+				Scheduler.addTimeTask(update, 0, 1 / 60);
+				System.notifyOnFrames(render);
 			});
 		});
 	}
