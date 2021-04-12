81cebec237ef0d9f85717f7d0d37a01f228747bb
Robert Konrad <git2016@robdangero.us>
2021-04-12 03:38:31 +0200

    Add the second test

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -20,13 +20,27 @@ class Main {
 		var context: KhaContext = vg.params.userPtr;
 		context.g = g;
 		NVG.nvgBeginFrame(vg, frames[0].width, frames[0].height, 1.0);
+		test1(vg);
+		test2(vg);
+		NVG.nvgEndFrame(vg);
+
+		g.end();
+	}
+
+	static function test1(vg: NVGcontext): Void {
 		NVG.nvgBeginPath(vg);
 		NVG.nvgRect(vg, 100, 100, 120, 30);
 		NVG.nvgFillColor(vg, NVG.nvgRGBA(255, 192, 0, 255));
 		NVG.nvgFill(vg);
-		NVG.nvgEndFrame(vg);
+	}
 
-		g.end();
+	static function test2(vg: NVGcontext): Void {
+		NVG.nvgBeginPath(vg);
+		NVG.nvgRect(vg, 100, 100, 120, 30);
+		NVG.nvgCircle(vg, 120, 120, 5);
+		NVG.nvgPathWinding(vg, NVGsolidity.NVG_HOLE); // Mark circle as a hole.
+		NVG.nvgFillColor(vg, NVG.nvgRGBA(255, 192, 0, 255));
+		NVG.nvgFill(vg);
 	}
 
 	public static function main() {
