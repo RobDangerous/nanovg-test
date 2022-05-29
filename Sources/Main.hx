3111615e74d974b5d4b94ad55381a3fc94754f7e
Robert Konrad <git2016@robdangero.us>
2022-05-30 01:01:57 +0200

    Update nanovg

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -20,9 +20,9 @@ class Main {
 		var context: KhaContext = vg.params.userPtr;
 		context.g = g;
 		NVG.nvgBeginFrame(vg, frames[0].width, frames[0].height, 1.0);
-		// test1(vg);
-		// test2(vg);
-		test3(vg);
+		//test1(vg);
+		test2(vg);
+		//test3(vg);
 		NVG.nvgEndFrame(vg);
 
 		g.end();
@@ -38,6 +38,7 @@ class Main {
 	static function test2(vg: NVGcontext): Void {
 		NVG.nvgBeginPath(vg);
 		NVG.nvgRect(vg, 100, 100, 120, 30);
+		//NVG.nvgRect(vg, 200, 200, 120, 30);
 		NVG.nvgCircle(vg, 120, 120, 5);
 		NVG.nvgPathWinding(vg, NVGsolidity.NVG_HOLE); // Mark circle as a hole.
 		NVG.nvgFillColor(vg, NVG.nvgRGBA(255, 192, 0, 255));
